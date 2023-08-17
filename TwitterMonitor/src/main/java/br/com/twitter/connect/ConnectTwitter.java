e62ee445750fb3a5e270e30fa6e21eb32b70f0ff
Jeovani Mendes <jeovani.mendes@gmail.com>
2023-08-17 16:35:02 +1000

    Update ConnectTwitter.java
    Changing the string connect.

--- TwitterMonitor/src/main/java/br/com/twitter/connect/ConnectTwitter.java
+++ TwitterMonitor/src/main/java/br/com/twitter/connect/ConnectTwitter.java
@@ -10,10 +10,10 @@ public class ConnectTwitter {
 	
 	public static Twitter oAuthManualTwitter () {
 		
-		String consumerKey = "9yEwBctD0ue4iryx5VsPmCLnl";
-		String consumerSecret = "SFv522grkHXY98ig2XFIVo2Qoh5yJH2BG1VsJzGy5ge6Ocfc5l";
-		String acessToken = "3050918313-J3tJKcZM78c4Wgq59CIMnp4MAjq8dJd3NI21Teq";
-		String tokenSecret = "ZJGj3djSE7FfbkCT5wHlycExXDdAhWXZB9RAJPYNe4X8G";
+		String consumerKey = "";
+		String consumerSecret = "";
+		String acessToken = "";
+		String tokenSecret = "";
 		
 		ConfigurationBuilder configBuilder = new ConfigurationBuilder().setDebugEnabled(true);
 		configBuilder.setOAuthConsumerKey(consumerKey);
